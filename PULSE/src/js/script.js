7f29cdca39820d7380bd077773a92d79355f4781
anvelichenko <anvelichenko@gmail.com>
2021-09-01 12:56:42 +0300

    1.09 домашка

--- PULSE/src/js/script.js
+++ PULSE/src/js/script.js
@@ -1,2 +1,19 @@
-let number = 4;
-console.log(number);
+const numberOffilms = prompt("Сколько фильмов вы уже посмотрели?", "20");
+console.log(numberOffilms);
+
+const personalMovieDB = {
+	count: numberOffilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+}; 
+
+const a = prompt("Один из последних просмотренных фильмов?", "Один дома");
+const b = prompt("Как оцените?", "10");
+const c = prompt("Один из последних просмотренных фильмов?", "Один дома 2");
+const d = prompt("Как оцените?", "8");
+
+
+	personalMovieDB.movies[a] = b;
+	personalMovieDB.movies[c] = d;
